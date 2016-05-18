a1a4aa273f0413ef8a93172cbe5ce91bb41172d7
Frannie-Ludmilla <frannie89.pesci@gmail.com>
2016-05-18 10:55:19 +0200

    First commit - empty project

---
+++ mavArch.sh
@@ -0,0 +1,9 @@
+ mvn archetype:generate\
+    -DarchetypeGroupId=org.apache.flink\
+    -DarchetypeArtifactId=flink-quickstart-scala\
+    -DarchetypeVersion=1.0.0\
+    -DgroupId=flink_knn_thyroid\
+    -DartifactId=flink_knn_thyroid\
+    -Dversion=0.1\
+    -Dpackage=thyroid\
+    -DinteractiveMode=false\
